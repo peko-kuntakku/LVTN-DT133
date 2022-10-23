05d80b89a431e190be4f231685db1943ba3911dd
Đặc Vụ Peko <74392254+peko-kuntakku@users.noreply.github.com>
2022-10-23 16:42:08 +0700

    Update BuildingForm.js

--- src/Building/BuildingForm.js
+++ src/Building/BuildingForm.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
 import React,{ useEffect, useState } from 'react';
 import {Routes, Route, useNavigate} from 'react-router-dom'
 import '../style/form.css'
 import {checkbox,input,dropdown} from "../style/JSfunc";
 import BuildingList from "./BuildingList";
+import {loadList, loadItem, addNewItem, updateItem, backend} from '../control';
 
 let budget = [
   {value: "electric", text: "Điện"},
@@ -12,8 +12,15 @@ let budget = [
   {value: "park", text: "Giữ xe"}
 ]
 
+let buildingID = 0;
+
+export const setBuildingID = (id) => 
+{
+  buildingID = id
+}
 
 function Main (){
+  const [building, loadBuilding]= useState();
   const [provinces, loadProvince]= useState([]);
   const [province, setProvince]= useState("");
   const [provincename, setProvinceName]= useState("");
@@ -23,8 +30,17 @@ function Main (){
   const [wards, loadWard]= useState([]);
   const [ward, setWard]= useState("");
   const [wardname, setWardName]= useState("");
+  const [locations, setLocations]= useState([]);
   const [f2, setF2]= useState(true);
   const [f3, setF3]= useState(true);
+
+  useEffect( ()=>{
+    loadItem('buildings', buildingID, loadBuilding);
+  },[buildingID]);
+
+  const Building_Name = (building!=null) ? building.attributes.Building_Name : null
+  const NumFloor = (building!=null) ? building.attributes.Num_of_Floors : null
+  const Description = (building!=null) ? building.attributes.Description : null
 
   //Lấy danh sách tỉnh thành
   useEffect( ()=>{
@@ -50,9 +66,13 @@ function Main (){
   //Lấy danh sách quận huyện
   useEffect( ()=>{
     const foo= async ()=>{
-      const a= await fetch(`https://provinces.open-api.vn/api/p/${province}?depth=2`);
-      const b= await a.json();
-      loadDistrict(await b.districts);
+      if (province!=null)
+      {
+        const a= await fetch(`https://provinces.open-api.vn/api/p/${province}?depth=2`);
+        const b= await a.json();
+        loadDistrict(await b.districts);
+      }
+      else return
     }
     foo();
   },[province]);
@@ -82,52 +102,43 @@ function Main (){
     setWard(event.target.value);
     event.preventDefault();
   }
-
-  const createNewBuilding = async (data) => {
-    await axios
-    .post("http://localhost:1337/api/buildings", data)
-  }
-
-  const createNewAddress = async (address) => {
-    await axios
-    .post("http://localhost:1337/api/locations", address)
-  }
-
+  useEffect(() => loadList('locations',setLocations) , [])
   const navigate = useNavigate();
   const handleClick = (event) => {
     event.preventDefault();
-    let {Building_Name, Num_of_Floors, Description} = document.forms[0];
-    const data = {
-      'data': {
-          "Building_Name": Building_Name.value,
-          "Num_of_Floors": Num_of_Floors.value,
-          "Description": Description.value
-      }
-    }
-    createNewBuilding(data);
     const address = {
-      'data': {
+      "data": {
           "Province": provincename,
           "District": districtname,
           "Ward": wardname,
           "Num": 10
       }
     }
-    console.log(address)
-    createNewAddress(address);
+    if (buildingID==0) addNewItem('locations', address);
+    let {Building_Name, NumFloor, Description} = document.forms[0];
+    const building = {
+      "data": {
+        "Building_Name": Building_Name.value,
+        "Num_of_Floors": NumFloor.value,
+        "Description": Description.value,
+        "location": locations.at(-1).id
+      }
+    }
+    if (buildingID==0) addNewItem('buildings', building)
+    else updateItem("buildings", buildingID, building)
     navigate('/Building/BuildingList');
   }
   const form = (
     <div>
-      <span className="function-title textxlsemibold">Thêm toà nhà</span>
+      <span className="function-title textxlsemibold">{(buildingID==0) ? "Thêm toà nhà" : `Chỉnh sửa tòa nhà BD ${buildingID}`}</span>
       <form onSubmit={handleClick} className="main-zone">
         <div className="big-row">
           <div className="col-left">
             <div className="item-area">
-              {input(2,"Building_Name","Tên","Tên toà nhà")}
+              {input(1,"Tên toà nhà","Building_Name",true,"Nhập tên",Building_Name, null, 1, 60, "",)}
             </div>
             <div className="item-area double-col">
-              {input(1,"Num_of_Floors","Nhập số tầng","Số tầng")}<div/>
+              {input(0,"Số tầng","NumFloor",true,"Nhập số tầng",NumFloor, null, 1, 70, 1)}<div/>
             </div>
             <div className="item-area">
               <span className="form-subtitle textmdsemibold">Dịch vụ</span>
@@ -142,18 +153,18 @@ function Main (){
           </div>
           <div className="col-left">
             <div className="item-area double-col">
-              {dropdown("Tỉnh, thành", "province", handleProvice, false, 
-              provinces, "code", "name", province, "Chọn tỉnh, thành", "Không có dữ liệu")}
+              {dropdown(1,"Tỉnh, thành","province",true,provinces,'',
+              province,handleProvice,false,"Chọn tỉnh, thành","Không có dữ liệu")}
               <div/>
             </div>
             <div className="item-area double-col">
-              {dropdown("Quận, huyện", "district", handleDistrict, f2, 
-              districts, "code", "name", district, "Chọn quận, huyện", "Vui lòng chọn tỉnh, thành")}
-              {dropdown("Phường, xã", "ward", handleWard, f3, 
-              wards, "code", "name", ward, "Chọn phường, xã", "Vui lòng chọn quận, huyện")}
+              {dropdown(1, "Quận, huyện", "district", true, districts, "",
+              district, handleDistrict, f2, "Chọn quận, huyện", "Vui lòng chọn tỉnh, thành")}
+              {dropdown(1, "Phường, xã", "ward", true, wards, "", 
+              ward, handleWard, f3, "Chọn phường, xã", "Vui lòng chọn quận, huyện")}
             </div>
             <div className="item-area">
-              {input(2,"num_street","Tên","Số, đường")}
+              {input(1,"Số, đường","num_street",true,"Tên",null, null, 1, 70, 1)}
             </div>
             <div className="item-area">
               <span className="form-subtitle textmdsemibold">Thêm ảnh</span>
