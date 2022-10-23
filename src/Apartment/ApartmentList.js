5a1737152c85091285ef4163152995363da80144
Đặc Vụ Peko <74392254+peko-kuntakku@users.noreply.github.com>
2022-10-23 17:37:12 +0700

    Update ApartmentList.js

--- src/Apartment/ApartmentList.js
+++ src/Apartment/ApartmentList.js
@@ -1,87 +1,175 @@
-import { func } from "prop-types";
 import React,{ useEffect, useState } from 'react';
-import {Routes, Route, Link, useNavigate} from 'react-router-dom';
-import ApartmentForm from "./ApartmentForm";
-import ApartmentDetail from "./ApartmentDetail";
-import '../style/main.css';
-import '../style/list.css';
+import {Routes, Route, Link, useNavigate} from 'react-router-dom'
+import ApartmentList from "./ApartmentList"
+import '../style/form.css'
+import {input,dropdown} from "../style/JSfunc";
+import {loadList, loadItem, addNewItem, updateItem, backend} from '../control';
 
-const colname = 
-[
-  {width: "12%", title: "Mã căn hộ"},
-  {width: "12%", title: "Tên căn hộ"},
-  {width: "12%", title: "Số phòng"},
-  {width: "12%", title: "Diện tích"},
-  {width: "22%", title: "Địa chỉ"},
-  {width: "12%", title: "Chi tiết"},
-  {width: "12%", title: "Cập nhật / Xoá"}
-]
+let apartmentID = 0;
+
+export const setApartmentID = (id) => 
+{
+  apartmentID = id
+}
 
 function Main () {
-  const [apartments, setApartments] = useState([]);
-  useEffect(() => {
-    fetch('http://localhost:1337/api/apartments')
-      .then((response) => response.json())
-      .then(({ data }) => setApartments(data)); // <-- save the data array
-  }, []);
+  const [apartment, loadApartment]= useState();
+  const [buildings, loadBuilding]= useState([]);
+  const [properties, loadProperties]= useState([]);
+  const [maxfloor, setFloor]= useState();
+  const [buildingname, setbuildingname]= useState();
+  const [f1, setF1]= useState(true);
+  const [numFloor, setNumFloor]= useState();
+  const [Livingroom, setLivingroom] = useState();
+  const [Kitchen, setKitchen] = useState();
+  const [Bedroom, setBedroom] = useState();
+  const [Restroom, setRestroom] = useState();
   const navigate = useNavigate();
-  const handleClick = (event) =>
+
+  useEffect( ()=>{
+    loadItem('apartments', apartmentID, loadApartment);
+  },[apartmentID]);
+
+  const Apartment_Name = (apartment!=null) ? apartment.attributes.Apartment_Name : null
+  const Size = (apartment!=null) ? apartment.attributes.Size : null
+  const Capacity = (apartment!=null) ? apartment.attributes.Capacity : null
+  const Rent_Fee = (apartment!=null) ? apartment.attributes.Rent_Fee : null
+  const Description = (apartment!=null) ? apartment.attributes.Description : null
+
+  let room = 
+  [
+    {name: "Livingroom", text: "Phòng khách", value: Livingroom, setVal: setLivingroom},
+    {name: "Kitchen", text: "Phòng bếp", value: Kitchen, setVal: setKitchen},
+    {name: "Bedroom", text: "Phòng ngủ", value: Bedroom, setVal: setBedroom},
+    {name: "Restroom", text: "Nhà vệ sinh", value: Restroom, setVal: setRestroom}
+  ]
+
+  useEffect(()=>{
+    if (apartment!=null) room.map(x=>x.setVal(apartment.attributes[x.name]))
+    else room.map(x=>x.setVal(0))
+  });
+
+  useEffect(() => {loadList('buildings',loadBuilding)
+    loadList('properties',loadProperties)} , [])
+  
+  const handleBuildingName = (event) =>
   {
+    setbuildingname(event.target.value);
+    setNumFloor("");
+    setF1(false);
     event.preventDefault();
-    navigate('/Apartment/ApartmentForm');
   }
-  return(
-    <div><span class="function-title textxlsemibold">Căn hộ</span>
-    <div class="main-zone">
-      <div class="main-head">
-        <div class="search-list">
-          <input
-            type="text"
-            placeholder="Tìm kiếm"
-            className="searchbar"
-          />
-          <img
-            src="/icon/search-icon.svg"
-            className="search-icon"
-          /> 
+  useEffect( ()=>{
+    const loadFloors= async ()=>{
+      if (buildingname!=null)
+      {
+        const a= await fetch(`${backend}/buildings/${buildingname}`);
+        const b= await a.json();
+        setFloor(await b.data);
+      }
+      else return
+    }
+    loadFloors();
+  },[buildingname]);
+  const floors = ((maxfloor!=null) ? Array.from({length: maxfloor.attributes.Num_of_Floors}, (_, i) => i + 1) :null);
+
+  const handleFloor = (event) =>
+  {
+    setNumFloor(event.target.value);
+    event.preventDefault();
+  }
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    let {Apartment_Name, Size, Capacity, Rent_Fee, Description} = document.forms[0];
+    const data = {
+      'data': {
+          "Apartment_Name": Apartment_Name.value,
+          "Size": Number(Size.value),
+          "Capacity": Number(Capacity.value),
+          "Rent_Fee": Rent_Fee.value,
+          "building": buildingname,
+          "Floor": Number(numFloor),
+          "Livingroom": Livingroom,
+          "Kitchen": Kitchen,
+          "Bedroom": Bedroom,
+          "Restroom": Restroom,
+          "Description": Description.value,
+          "isSingle": true
+      }
+    }
+    if (apartmentID==0) addNewItem("apartments", data)
+    else updateItem("apartments", apartmentID, data)
+    navigate('/Apartment/ApartmentList');
+  }
+  const form = (
+    <>
+      <span className="function-title textxlsemibold">{(apartmentID==0) ? "Thêm căn hộ" : `Chỉnh sửa căn hộ AP ${apartmentID}`}</span>
+      <form onSubmit={handleClick} className="main-zone">
+        <div className="big-row">
+          <div className="col-left">
+            <div className="item-area">
+              {input(1,"Tên căn hộ","Apartment_Name",true,"Tên",Apartment_Name,null,1,60,"",)}
+            </div>
+            <div className="item-area double-col">
+              {input(0,"Diện tích","Size",true,"Đơn vị: m2",Size, null, 1, 1000, 0.5)}
+              {input(0,"Sức chứa","Capacity",true,"Nhập số người",Capacity, null, 1, 20, 1)}
+            </div>
+            <div className="item-area double-col">
+              {input(0,"Tiền thuê","Rent_Fee",true,"Nhập số tiền",Rent_Fee, null, 10000, "", 500)}
+              <div/>
+            </div>
+            <div className="item-area double-col">
+              {dropdown(0,"Toà nhà","building_name",true, buildings, "Building_Name",
+              buildingname,handleBuildingName,false,"Chọn tòa nhà","Chọn tòa nhà")}
+              {dropdown(2,"Tầng","Floor", true, floors, "",
+              numFloor,handleFloor,f1,"Chọn số tầng","Vui lòng chọn tòa nhà")}
+            </div>
+            <div className="item-area">
+              <span className="form-subtitle textmdsemibold">Mô tả</span>
+              <textarea name="Description" className="text-input" placeholder="Mô tả" defaultValue={Description}></textarea>
+            </div>
+          </div>
+          <div className="col-left">
+            <div className="item-area">
+              <span className="form-subtitle textmdsemibold">Phòng (nhập số lượng)</span>
+              {/* <span className="form-subtitle textmdsemibold" 
+              style={{algin: "right", float:"right", right: "40px", position: "relative"}}>
+                Tổng: {Livingroom + Kitchen + Bedroom + Restroom}</span> */}
+              <div className="double-col">{room.map((x) => input(0,'',x.name,false,x.text,x.value,x.setVal,0,5,1))}</div>
+            </div>
+            <div className="item-area">
+              <span className="form-subtitle textmdsemibold">Nội thất (nhập số lượng)</span>
+              <div className="double-col">{properties.map((x) =>input(0,'',x.id,false,x.attributes.Property_Name,null,null,0,10,1))}</div>
+            </div>
+            <div className="item-area">
+              <span className="form-subtitle textmdsemibold">Thêm ảnh</span>
+              <div className="add-file-area">
+                <input type="file" name="myfile" id="myfile" hidden/>
+                <label for="myfile" className="add-file-button">Nhấn để thêm ảnh</label>
+                </div>
+            </div>
+          </div>
         </div>
-        <button class="addbutton" onClick={handleClick}>
-          <span class="addbutton-text textsmsemibold">Thêm căn hộ</span>
-          <img
-            src="./icon/add-icon.svg"
-            class="pluscircle" />
-        </button>
-      </div>
-      <div className="table-area">
-        <table>
-          <tr className="rowtitle">
-            {colname.map((a)=><th className="textsm" style={{width: a.width}}>{a.title}</th>)}
-          </tr>
-          {apartments.map(data => 
-          <tr>
-            <td className="textsm">{data.id}</td>
-            <td className="textsm">{data.attributes.Apartment_Name}</td>
-            <td className="textsm">{data.attributes.Livingroom + data.attributes.Bedroom + data.attributes.Kitchen + data.attributes.Restroom}</td>
-            <td className="textsm">{data.attributes.Size}</td>
-            <td className="textsm">{}</td>
-            <td className="textsm"><Link to='/BuildingDetail'>Xem chi tiết</Link></td>
-            <td className="textsm"><img alt="edit4140" src="/icon/edit-icon.svg" className="edit-icon"/>
-              <img alt="trashalt4140" src="/icon/delete-icon.svg" className="trash-icon" />
-            </td>
-          </tr>)}
-        </table>
-      </div>
-    </div></div>
+        <div className="submit-area">
+          <button type="submit" className="submit-button">
+            <span className="submit-btntxt textsmsemibold">{(apartmentID==0) ? "Thêm căn hộ" : "Cập nhật"}</span>
+          </button>
+        </div>
+      </form>
+    </>
+  )
+  return (
+    <>{form}</>
   )
 }
 
-export default function ApartmentList ()
+export default function ApartmentForm () 
 {
   return(
     <Routes>
       <Route path="/" element={<Main />} />
-      <Route path="/Apartment/ApartmentForm" element={<ApartmentForm />} />
-      <Route path="/Apartment/ApartmentDetail" element={<ApartmentDetail />} />
+      <Route path="/Apartment/ApartmentList" element={<ApartmentList />} />
     </Routes>
   )
 }
