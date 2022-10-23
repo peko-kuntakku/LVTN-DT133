0d30b70f89d62971f808693f09fc21fb8490bc7b
Đặc Vụ Peko <74392254+peko-kuntakku@users.noreply.github.com>
2022-10-23 16:36:32 +0700

    Update BuildingList.js

--- src/Building/BuildingList.js
+++ src/Building/BuildingList.js
@@ -5,6 +5,7 @@ import '../style/main.css';
 import '../style/list.css';
 import BuildingForm from "./BuildingForm";
 import BuildingDetail from "./BuildingDetail";
+import { loadList, deleteItem } from '../control';
 
 const colname = 
 [
@@ -18,18 +19,18 @@ const colname =
 
 function Main () {
   const [buildings, setBuilding] = useState([]);
-  useEffect(() => {
-    axios
-      .get('http://localhost:1337/api/buildings')
-      .then(({ data }) => setBuilding(data.data))
-  }, [])
-  
+  useEffect(() => {loadList('buildings',setBuilding)}, [])
   const navigate = useNavigate();
-  const handleClick = (event) =>
+  const handleEdit = (event) => {
+    event.preventDefault();
+    navigate('/Building/BuildingForm');
+  };
+  const handleAdd = (event) =>
   {
     event.preventDefault();
     navigate('/Building/BuildingForm');
   }
+
   return(
     <div>
       <span class="function-title textxlsemibold">Toà nhà</span>
@@ -46,7 +47,7 @@ function Main () {
               className="search-icon"
             /> 
           </div>
-          <button class="addbutton" onClick={handleClick}>
+          <button class="addbutton" onClick={handleAdd}>
               <span class="addbutton-text textsmsemibold">Thêm toà nhà</span>
               <img
                 src="./icon/add-icon.svg"
@@ -59,15 +60,15 @@ function Main () {
             <tr className="rowtitle">
               {colname.map((a)=><th className="textsm" style={{width: a.width}}>{a.title}</th>)}
             </tr>
-            {buildings.map(data => 
+            {buildings.map(({id, attributes}) => 
             <tr>
-              <td className="textsm">{data.id}</td>
-              <td className="textsm">{data.attributes.Building_Name}</td>
-              <td className="textsm">{data.attributes.Num_of_Floors}</td>
-              <td className="textsm">{data.attributes.Type}</td>
+              <td className="textsm">{id}</td>
+              <td className="textsm">{attributes.Building_Name}</td>
+              <td className="textsm">{attributes.Num_of_Floors}</td>
+              <td className="textsm">{}</td>
               <td className="textsm"><Link to='/Building/BuildingDetail'>Xem chi tiết</Link></td>
-              <td className="textsm"><img alt="edit4140" src="/icon/edit-icon.svg" className="edit-icon"/>
-                <img alt="trashalt4140" src="/icon/delete-icon.svg" className="trash-icon" />
+              <td className="textsm"><img alt="edit4140" src="/icon/edit-icon.svg" className="edit-icon" onClick={handleEdit}/>
+                <img alt="trashalt4140" src="/icon/delete-icon.svg" className="trash-icon" onClick={()=>deleteItem('buildings', id)}/>
               </td>
             </tr>)}
           </table>
