3298c775062f13390583b81b188f7a7de253aa02
Đặc Vụ Peko <74392254+peko-kuntakku@users.noreply.github.com>
2022-10-23 17:30:23 +0700

    Update JSfunc.js

--- src/style/JSfunc.js
+++ src/style/JSfunc.js
@@ -8,8 +8,8 @@ export const choice = (title, name, list) =>
     <div className="choice-area">
       {list.map((x) => 
       <div className="checkbox-item">
-        <input type="radio" className="choice-icon" name={name} value={list.value} id={list.value} />
-        <label for id={list.value} className="checkbox-text textsm">{list.text}</label>
+        <input type="radio" className="choice-icon" name={name} value={x.code} id={x.code} required/>
+        <label for id={x.code} className="checkbox-text textsm">{x.name}</label>
       </div>)}
     </div></>
   )
@@ -25,47 +25,59 @@ export const checkbox = (name, value, text) =>
   )
 }
 
-export function dropdown (title, name, handle, isDisable, list, kind, result, holder1, holder2){
+export function dropdown (type, title, name, required, list, subName, result, setValue, isDisable, holder1, holder2){
   const placeholder = (list, holder1, holder2) => 
   {
     if ((list!=undefined))
     return (<option value="" selected disabled>{holder1}</option>)
-    else return (<option value="" disabled selected>{holder2}</option>)
+    else return (<option value="" selected disabled>{holder2}</option>)
   }
-  const renderlist = (list, kind) =>
+  const renderlist = (list, type, subName) =>
   {
     if (list!=undefined) 
     {
-      if (kind=="address")
+      if (type==1)
       return list.map((x) => <option value={x.code}>{x.name}</option>)
-      else if (kind=="building")
-      return list.map((x) => <option value={x.id}>{x.attributes.Building_Name}</option>)
-      else if (kind=="floor")
-      return list.map((x) => <option value={x}>{x}</option>)
+      else if (type==0)
+      return list.map((x) => <option value={x.id}>{x.attributes[subName]}</option>)
+      else if (type==2)
+      return list.map((x) => <option value={x}>Tầng {x}</option>)
     }
   }
   return(
   <>
   <span className="form-subtitle textmdsemibold">{title}</span>
-  <select name={name} className="form-select" disabled={isDisable} onChange={(e) => handle(e)} value={result}>
+  <select name={name} className="form-select" required={required} disabled={isDisable} 
+    onChange={(e) => {if (setValue!=null) setValue(e)}} value={result}>
     {placeholder (list, holder1, holder2)}
-    {renderlist (list, kind)}
+    {renderlist (list, type, subName)}
   </select>
   </>
   )
 }
 
-export const input = (type, name, placeholder, title) => 
+export const input = (type, title, name, required, placeholder, result, setValue, min, max, step) => 
 {
+  // type : 0-number, 1- text, 2-date, 3-time, 4-password
   return(
     <><span className="form-subtitle textmdsemibold">{title}</span>
     <input
+      onInput = {(e) => {if (setValue!=null) {
+        if ((type==0)) setValue(Number(e.target.value)) 
+        else setValue(e.target.value)}}}
       name={name}
-      type={(type==1) ? "number" : (type==2) ? "text" : (type==3) ? "date" : "time"}
-      min={(type==1) ? "0" : ""}
+      type={(type==1) ? "text" : (type==2) ? "date" : (type==3) ? "time" : (type==4) ? "tel" : "number"}
+      required={required}
+      min={((type==0)||(type==2)) ? min : null}
+      minLength={(type==1) ? min : null}
+      max={((type==0)||(type==2)) ? max : null}
+      maxLength={(type==1) ? max : null}
+      step={(type==0) ? step : null}
+      pattern={(type==4) ? step : null}
       placeholder={placeholder}
+      defaultValue={result}
       className="form-input"
-      style={{width: (type==2) ? "95%" : "90%"}}  />
+      style={{width: (type==1) ? "95%" : "90%"}}  />
     </>
   )
 }
