dccae430dd2f8aa6602d593b2b5b4c5eb607cf26
Đặc Vụ Peko <74392254+peko-kuntakku@users.noreply.github.com>
2022-10-24 12:12:14 +0700

    Update control.js

--- src/control.js
+++ src/control.js
@@ -1,23 +1,18 @@
 import axios from 'axios';
 
 export const backend = 'http://localhost:1337/api'
-
-export const loadList = async (category, loadCommand)=>
-{
-  await axios.get(`${backend}/${category}`)
-  .then(({ data }) => loadCommand(data.data))
-}
+export const ProvinceAPI = 'https://provinces.open-api.vn/api'
 
 export const loadItem = async (category, Id, loadCommand)=>
 {
-  if (Id!=0)
+  if (Id===0) return
+  else
   {
     await axios.get(`${backend}/${category}/${Id}`)
     .then(({ data }) => loadCommand(data.data))
   }
-  else return
 }
-export const addNewItem = async (category, data) => {
+export const addNewItem = async (m, category, data) => {
   await axios.post(`${backend}/${category}`, data)
 };
 export const updateItem = async (category, Id, data) => {
@@ -26,3 +21,10 @@ export const updateItem = async (category, Id, data) => {
 export const deleteItem = async (category, Id) => {
   await axios.delete(`${backend}/${category}/${Id}`)
 };
+export const loadLocation = async (type,loadCommand,Id,typename) =>
+{
+  console.log(`${ProvinceAPI}/${type}/${Id}${(Id=='') ? '' : '?depth=2'}`)
+  axios.get(`${ProvinceAPI}/${type}/${Id}${(Id=='') ? '' : '?depth=2'}`)
+  .then(({ data }) => {if (typename==null) loadCommand(data); else loadCommand(data[typename])})
+}
+
