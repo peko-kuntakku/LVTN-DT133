3e19a9e23cbf1afaa3673c51335434c44f4f9fc5
Đặc Vụ Peko <74392254+peko-kuntakku@users.noreply.github.com>
2022-10-26 21:10:07 +0700

    Update BuildingList.js

--- src/Building/BuildingList.js
+++ src/Building/BuildingList.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
 import React,{ useEffect, useState } from 'react';
 import {Routes, Route, Link, useNavigate} from 'react-router-dom';
 import '../style/main.css';
 import '../style/list.css';
-import BuildingForm from "./BuildingForm";
-import BuildingDetail from "./BuildingDetail";
-import { loadList, deleteItem } from '../control';
+import BuildingDetail,{ setBuildingID2 } from "./BuildingDetail";
+import { deleteItem, loadItem, loadLocation } from '../control';
+import BuildingForm, { setBuildingID } from './BuildingForm';
+import axios from 'axios';
 
 const colname = 
 [
@@ -17,17 +17,34 @@ const colname =
   {width: "14%", title: "Cập nhật / Xoá"}
 ]
 
+// function showLocate(p)
+// {
+//   const [Province, setProvince]= useState('');
+//   const [District, setDistrict]= useState('');
+//   const [Ward, setWard]= useState('');
+//   loadLocation('p',p.Province,setProvince)
+//   loadLocation('d',p.District,setDistrict)
+//   loadLocation('w',p.Ward,setWard)
+//   return Province
+// }
+
 function Main () {
-  const [buildings, setBuilding] = useState([]);
-  useEffect(() => {loadList('buildings',setBuilding)}, [])
+  const [buildings, setBuildings] = useState([]);
+  useEffect(() => {loadItem('buildings','',setBuildings,'location')}, [])
+
   const navigate = useNavigate();
-  const handleEdit = (event) => {
-    event.preventDefault();
+  const handleDetail = (id) => {
+    setBuildingID2(id)
+    navigate('/Building/BuildingDetail');
+  };
+  const handleEdit = (id) => {
+    setBuildingID(id)
     navigate('/Building/BuildingForm');
   };
   const handleAdd = (event) =>
   {
     event.preventDefault();
+    setBuildingID(0);
     navigate('/Building/BuildingForm');
   }
 
@@ -63,11 +80,11 @@ function Main () {
             {buildings.map(({id, attributes}) => 
             <tr>
               <td className="textsm">{id}</td>
-              <td className="textsm">{attributes.Building_Name}</td>
+              <td className="textsm">{attributes.BuildingName}</td>
               <td className="textsm">{attributes.Num_of_Floors}</td>
-              <td className="textsm">{}</td>
-              <td className="textsm"><Link to='/Building/BuildingDetail'>Xem chi tiết</Link></td>
-              <td className="textsm"><img alt="edit4140" src="/icon/edit-icon.svg" className="edit-icon" onClick={handleEdit}/>
+              <td className="textsm">{/*showLocate(attributes.location.data.attributes)*/}</td>
+              <td className="textsm"><span onClick={()=>handleDetail(id)}>Xem chi tiết</span></td>
+              <td className="textsm"><img alt="edit4140" src="/icon/edit-icon.svg" className="edit-icon" onClick={()=>handleEdit(id)}/>
                 <img alt="trashalt4140" src="/icon/delete-icon.svg" className="trash-icon" onClick={()=>deleteItem('buildings', id)}/>
               </td>
             </tr>)}
